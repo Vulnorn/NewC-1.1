a9db71067655d6f1e2e43a11e22c901a09893460
Vulnorn <mixadrbk31@gmail.com>
2024-09-23 13:33:12 +0300

    HW

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
-﻿byte age;
-string name;
-bool isAbult;
-float weight;
+﻿byte age =30;
+string name = "Mikhail";
+bool isAbult = true;
+double weight = 73.5;
 int abult = 18;
-string message;
+string message = "Привет, как дела!";
+int day = 23;
+string month = "September";
+int year = 2024;
+string description = $"Сегодня {day} {month} {year} года.";
+
+Console.WriteLine(description);
+Console.ReadKey();
